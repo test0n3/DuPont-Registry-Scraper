bb15d95ca1cd5d50d8964ef56d2c4fee09f0b28a
Carlos A <cayala.w@gmail.com>
2019-11-03 19:45:59 -0500

    Update with styles

--- scraper/lib/models/scraper.rb
+++ scraper/lib/models/scraper.rb
@@ -1,84 +1,101 @@
+# frozen_string_literal: true
+
 require 'nokogiri'
 require 'rest-client'
 require 'httparty'
 require 'byebug'
 require 'pry'
 
+# class Scraper, capture data from specific URL and captures relevant info.
 class Scraper
+  attr_reader :url, :make, :model
 
-    attr_reader :url, :make, :model
+  def initialize(make, model)
+    @make = make.capitalize
+    @model = model.capitalize
+    @url = "https://www.dupontregistry.com/autos/results/#{make}/#{model}/for-sale"
+           .sub(' ', '--')
+  end
 
-    def initialize (make, model)
-     @make = make.capitalize
-     @model = model.capitalize
-     @url = "https://www.dupontregistry.com/autos/results/#{make}/#{model}/for-sale".sub(" ", "--")
-    end 
-
-def parse_url(url)
+  def parse_url(url)
     unparsed_page = HTTParty.get(url)
     Nokogiri::HTML(unparsed_page)
-end 
+  end
 
- def scrape 
- parsed_page = parse_url(@url)
+  def scrape
+    parsed_page = parse_url(@url)
+    # Nokogiri object containing all cars on a given page
+    cars = parsed_page.css('div.searchResults')
+    puts "Parsed_page is #{cars.class}"
+    # counts the number of cars on each page, should be 10
+    per_page = cars.count
+    # total_listings recovers total amount of results
+    total_listings = parsed_page
+                     .css('#mainContentPlaceholder_vehicleCountWithin')
+                     .text.to_i
+    # returns number of pages the search results are distributed
+    total_pages = self.get_number_of_pages(total_listings, per_page)
 
-  cars = parsed_page.css('div.searchResults') #Nokogiri object containing all cars on a given page
+    first_page = create_car_hash(cars)
+    all_other = build_full_cars(total_pages)
+    first_page + all_other.flatten
+    binding.pry
+  end
+  # binding.pry
+  # 0
 
-  per_page = cars.count #counts the number of cars on each page, should be 10
-  total_listings = parsed_page.css('#mainContentPlaceholder_vehicleCountWithin').text.to_i
-  total_pages = self.get_number_of_pages(total_listings, per_page)
-
- first_page = create_car_hash(cars)
- all_other = build_full_cars(total_pages)
- first_page + all_other.flatten
- binding.pry
-
- end 
-
- def create_car_hash(car_obj)
-
-    car_obj.map { |car|
-
-    {
-        year: car.css('a').children[0].text[0..4].strip.to_i,
+  # creates a hash with the values we need from ourparsed car object
+  def create_car_hash(car_obj)
+    # puts "car_obj content: #{car_obj}"
+    puts "what is car_obj: #{car_obj.class}"
+    car_obj.map do |car|
+      {
+        year: car.css('a')
+                 .children[0]
+                 .text[0..4]
+                 .strip
+                 .to_i,
         name: @make,
         model: @model,
-        price: car.css('.cost').children[1].text.sub(",","").to_i,
-        link: "https://www.dupontregistry.com/#{car.css('a').attr('href').value}"
-    }
+        price: car.css('.cost')
+                  .children[1]
+                  .text.sub(',', '')
+                  .to_i,
+        link: "https://www.dupontregistry.com/#{car.css('a')
+                                                   .attr('href')
+                                                   .value}"
+      }
+    end
+  end
 
-   }
+  # gets URLs of all pages, not just the first page
+  def get_all_page_url(array_of_ints)
+    array_of_ints.map do |number|
+      @url + "/pagenum=#{number}"
+    end
+  end
 
-end 
+  # finds how many pages of listings exist
+  def get_number_of_pages(listings, cars_per_page)
+    a = listings % cars_per_page
+    if a.zero?
+      listings / cars_per_page
+    else
+      listings / cars_per_page + 1
+    end
+  end
 
-def get_all_page_urls(array_of_ints)
-    array_of_ints.map { |number| 
-     @url + "/pagenum=#{number}"
-  }
-end 
+  # builds an array of car hashes for each page of the listings,
+  # starting on page 2
+  def build_full_cars(number_of_pages)
+    a = [*2..number_of_pages]
+    all_page_urls = get_all_page_url(a)
 
-def get_number_of_pages(listings, cars_per_page)
- a = listings % cars_per_page
- if a == 0
-    listings / cars_per_page
- else 
-    listings / cars_per_page + 1
- end 
-
-end 
-
-def build_full_cars(number_of_pages)
- a = [*2..number_of_pages]
- all_page_urls = get_all_page_urls(a)
-
- all_page_urls.map { |url| 
- pu = parse_url(url)
- cars = pu.css('div.searchResults')
- create_car_hash(cars)
-}
-
-end 
-
-binding.pry
-
-end 
+    all_page_urls.map do |url|
+      pu = parse_url(url)
+      cars = pu.css('div.searchResults')
+      create_car_hash(cars)
+    end
+  end
+  binding.pry
+end
